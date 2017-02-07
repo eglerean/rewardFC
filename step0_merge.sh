2e1aee069621002940f74d5dcbd49c3d69370865
Enrico Glerean <enrico.glerean@aalto.fi>
2017-02-07 13:15:01 +0200

    dry run for step0

--- step0_merge.sh
+++ step0_merge.sh
@@ -1,30 +1,30 @@
-basepath='/scratch/braindata/eglerean/motfmri/niidata/';
-for n in $(seq 4 26); do
-	if [ -d aa$n ]; then
-		echo "Skipping $n"
-	else
-		#extract in temp
-		echo tar xvf $basepath$n.tar -C $basepath/temp/
-		tar xvf $basepath$n.tar -C $basepath/temp/
-		# process mot1 to 3
-		for m in $(seq 1 3); do
-			echo mkdir -p $basepath/$n/
-			mkdir -p $basepath/$n/
-			id=$(find $basepath/temp/$n/mitmot$m|grep sw.*0001.nii)
-			ide=$(echo $id|sed 's/0001.nii//g');
-			mot=$(find $basepath/temp/$n/mitmot$m|grep rp.*txt);
-			echo cp $mot $basepath/$n/$m.txt
-			cp $mot $basepath/$n/$m.txt
-			echo /scratch/braindata/eglerean/motfmri/git/merge_it.sh $ide 1 553 $basepath/$n/$m.nii  			
-			/scratch/braindata/eglerean/motfmri/git/merge_it.sh $ide 1 553 $basepath/$n/$m.nii
-			if [ $? -ne 0 ]; then
-				echo "error. stopping"
-				exit 1	
-			fi  
-			echo gunzip $basepath/$n/$m.nii.gz		
-			gunzip $basepath/$n/$m.nii.gz
-		done
-		echo rm -r temp/*
-		rm -r temp/*
-	fi
+#!/bin/bash
+basepath='/home/scratch/lauri/RewardPET/';
+outpath='/home/scratch/eglerean/dataout/';
+
+# set FSL output to NII without gz
+export FSLOUTPUTTYPE=NIFTI
+
+# First we find which folders contain data. There are two runs per subject. Each run has length 430
+find $basepath -type f -name "sw*_430.nii"|sort -n|cut -d\/ -f1-8 > $outpath/folderlist.txt
+
+subNum=1;
+for f in $(cat $outpath/folderlist.txt); do
+	# get subject ID from the study, needed for the regressors
+	subjID=$(echo $f|cut -d\/ -f7);
+	# get the run number for the subject
+	runID=$(echo $f|cut -d\/ -f8|sed 's/[A-z]*//g')
+	echo mkdir -p $outpath/$subNum/$runID/
+	# identify string for subject data
+	id=$(find $f|grep sw.*0001.nii);
+	ide=$(echo $id|sed 's/0001.nii//g');
+	# identify motion parameters
+	mot=$(find $f|grep rp.*txt);
+	echo cp $mot $outpath/$subNum/$runID/
+	echo /scratch/braindata/eglerean/motfmri/git/merge_it.sh $ide 1 430 $outpath/$subNum/$runID/epi_prerocessed.nii  			
+	if [ $? -ne 0 ]; then
+		echo "error. stopping"
+		exit 1	
+	fi  
+	let subNum=subNum+1;
 done
